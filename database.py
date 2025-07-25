fdc1f99841b6950522fa9513a99dfd16c8cbe84b
JuegosYCodigos <lgandomidios8@gmail.com>
2025-07-25 16:02:33 -0400

    fix: update database connection

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-#DATABASE_URL = os.getenv("postgresql://db_dragonball_user:f1KpFbbOFTQUTtYBs0ifw2uwdIA89qCS@dpg-d21g0qvgi27c73dsekg0-a.oregon-postgres.render.com/db_dragonball") 
-DATABASE_URL = "postgresql://db_dragonball_user:f1KpFbbOFTQUTtYBs0ifw2uwdIA89qCS@dpg-d21g0qvgi27c73dsekg0-a.oregon-postgres.render.com/db_dragonball"
+DATABASE_URL = os.getenv("postgresql://db_dragonball_user:f1KpFbbOFTQUTtYBs0ifw2uwdIA89qCS@dpg-d21g0qvgi27c73dsekg0-a.oregon-postgres.render.com/db_dragonball") 
+#DATABASE_URL = "postgresql://db_dragonball_user:f1KpFbbOFTQUTtYBs0ifw2uwdIA89qCS@dpg-d21g0qvgi27c73dsekg0-a.oregon-postgres.render.com/db_dragonball"
 
 engine = create_engine(DATABASE_URL)
 
